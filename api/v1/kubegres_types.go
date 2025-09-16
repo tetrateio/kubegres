169b096b5fb99794d0a53216ce916db3e008fa72
Piotr Kopec <piotr@tetrate.io>
2025-09-16 12:11:08 +0200

    fix default
    Signed-off-by: Piotr Kopec <piotr@tetrate.io>

--- api/v1/kubegres_types.go
+++ api/v1/kubegres_types.go
@@ -83,7 +83,7 @@ type ReplicationSlots struct {
 	// +kubebuilder:validation:Pattern:="^([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$"
 	// +kubebuilder:default:="10m"
 	// InactiveSlotGracePeriod defines how long a replication slot can remain inactive before it becomes eligible for cleanup.
-	// The value should be specified in the format: 30s, 1m, 5m, etc. Default is 2m.
+	// The value should be specified in the format: 30s, 1m, 5m, etc. Default is 10m.
 	InactiveSlotGracePeriod metav1.Duration `json:"inactiveSlotGracePeriod,omitempty"`
 	// +kubebuilder:validation:Type:=string
 	// +kubebuilder:validation:Pattern:="^([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$"
